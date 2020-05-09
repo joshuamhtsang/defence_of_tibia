21b8262babd72fa1ae6c6e8a5012a5beec73f225
joshuamhtsang <joshua.mh.tsang@gmail.com>
2020-05-09 18:59:28 +0100

    add a population mechanic

--- src/App.js
+++ src/App.js
@@ -9,11 +9,24 @@ function App() {
   const [castleMaxHealth, setCastleMaxHealth] = useState(100);
   const [castleHealth, setCastleHealth] = useState(80);
   const [gold, setGold] = useState(1000);
+  const [citizens, setCitizens] = useState(100);
 
   useEffect(() => {
     let interval = null;
     interval = setInterval(() => {
-      setTurn(turn => turn + 1)
+      setTurn(turn => turn + 1);
+
+      const income_period = 10;
+      if (turn % income_period === 0 && turn != 0) {
+        let income = Math.round(citizens * 0.1);
+        setGold(gold => gold + income);
+      }
+
+      const population_increase_period = 100;
+      if (turn % population_increase_period === 0 && turn != 0) {
+        let population_increase = Math.round(citizens * 0.05);
+        setCitizens(citizens => citizens + population_increase);
+      }
     }, 250);
 
     return () => clearInterval(interval);
@@ -26,6 +39,7 @@ function App() {
       <div> Name: {characterName} </div>
       <div> Turn: {turn} </div>
       <div> Gold: {gold} </div>
+      <div> Citizens: {citizens}</div>
 
       <FillableBar 
         value={castleHealth}
@@ -35,7 +49,7 @@ function App() {
       />
 
       <BattleField>
-        
+
       </BattleField>
     </div>
   )
