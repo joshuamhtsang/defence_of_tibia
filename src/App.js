ad97b5d9939a1c84f22e2ab7d5220d82d665723c
joshuamhtsang <joshua.mh.tsang@gmail.com>
2020-05-09 15:34:02 +0100

    cleaner out default main class component, and replace with functional component with state hooks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h2>Welcome to React</h2>
+//         </div>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+function App() {
+const [characterName, setCharacterName] = useState('Ashe');
+
+  return (
+    <div className="App"> 
+      <div> Defence of Tibia </div>
+      <div> Name: {characterName} </div>
+    </div>
+  )
 }
 
 export default App;
