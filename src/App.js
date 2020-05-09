5e620c716ede5ad0cfc791fa0d5194eed34a5678
joshuamhtsang <joshua.mh.tsang@gmail.com>
2020-05-09 15:51:07 +0100

    added turn counter

--- src/App.js
+++ src/App.js
@@ -19,12 +19,26 @@ import './App.css';
 // }
 
 function App() {
-const [characterName, setCharacterName] = useState('Ashe');
+  const [characterName, setCharacterName] = useState('Ashe');
+  const [turn, setTurn] = useState(0);
+  const [castleHealth, setCastleHealth] = useState(100);
+  const [gold, setGold] = useState(1000);
+
+  useEffect(() => {
+    let interval = null;
+    interval = setInterval(() => {
+      setTurn(turn => turn + 1)
+    }, 250);
+
+    return () => clearInterval(interval);
+  }
+  );
 
   return (
     <div className="App"> 
       <div> Defence of Tibia </div>
       <div> Name: {characterName} </div>
+      <div> Turn: {turn}</div>
     </div>
   )
 }
