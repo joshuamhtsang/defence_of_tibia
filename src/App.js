f9fbde5cbd0b2022f22cb7bf6fd161c9eb5b03ad
joshuamhtsang <joshua.mh.tsang@gmail.com>
2020-05-09 15:58:20 +0100

    removed unwanted css

--- src/App.js
+++ src/App.js
@@ -2,22 +2,6 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
 function App() {
   const [characterName, setCharacterName] = useState('Ashe');
   const [turn, setTurn] = useState(0);
